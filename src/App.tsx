a16cf0a4c7b3b949eed2cfe986c049c1803d4994
Han jin Park <hjpark@Hanui-MacBookPro.local>
2022-07-23 11:58:55 +0900

    Concept of get, set in Recoil Selector

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,23 @@
 import React from 'react';
 import { GlobalStyle } from './style/GlobalStyle';
+import { useRecoilState } from 'recoil';
+import { hourSelector, minuteState } from './atoms';
 
 function App() {
+  const [min, setMin] = useRecoilState(minuteState);
+  const [hour, setHour] = useRecoilState(hourSelector);
+
+  const onMinChange = (e:React.FormEvent<HTMLInputElement>) => {
+    setMin(+e.currentTarget.value)
+  }
+  const onHourChange = (e:React.FormEvent<HTMLInputElement>) => {
+    setHour(+e.currentTarget.value)
+  }
   return (
     <div className="App">
       <GlobalStyle />
+      <input type="number" placeholder='number' value={min} onChange={onMinChange} />
+      <input type="number" placeholder='hours' value={hour} onChange={onHourChange} />
     </div>
   );
 }
