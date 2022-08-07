107456596656a021565267c4330b42377992adda
Han jin Park <phj9020@hotmail.com>
2022-08-07 12:21:22 +0900

    moving card color applied

--- src/Components/DragabbleCard.tsx
+++ src/Components/DragabbleCard.tsx
@@ -4,11 +4,12 @@ import styled from 'styled-components';
 
 
 
-const Card = styled.div`
+const Card = styled.div<{isDragging: boolean}>`
 border-radius: 5px;
 padding: 10px;
-background-color: ${(props) => props.theme.cardColor};
+background-color: ${(props) => props.isDragging ? "#74b9ff" : props.theme.cardColor};
 margin-bottom: 5px;
+box-shadow : ${props => props.isDragging ? "0px 2px 5px rgba(0,0,0,0.05)" : "none"};
 `
 
 interface IDraggableCardProps {
@@ -20,8 +21,9 @@ function DraggableCard({todo, index}: IDraggableCardProps) {
     console.log(todo, "has been rendered")
     return (
         <Draggable key={todo} draggableId={todo} index={index}>
-            {(provided) =>
+            {(provided, snapshot) =>
                 <Card
+                    isDragging={snapshot.isDragging}
                     ref={provided.innerRef}
                     {...provided.draggableProps}
                     {...provided.dragHandleProps}>
